2a89e0c28aa332d45d636848c1aa9b28a91702cd
k-westbrook <kcheriewestbrook@gmail.com>
2019-11-22 12:17:27 -0800

    added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Routes from './Routes.js'
+import { BrowserRouter as Router } from 'react-router-dom'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Router>
+          <Routes />
+        </Router>
         <fieldset>
           <legend>Welcome, Edd is Old</legend>
           <h1>Happy Birthday Edd!</h1>
---
+++ src/Routes.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
+
+class Routes extends Component {
+
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+
+        <Switch >
+          <Route exact path="/" component={HomePage} />
+
+        </Switch>
+
+
+      </div>
+    )
+  }
+}
+
+export default Routes;
