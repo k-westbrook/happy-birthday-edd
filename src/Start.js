bc8c8ace4468e500f86dd785252c3e6ba296def6
k-westbrook <kcheriewestbrook@gmail.com>
2019-11-22 13:32:40 -0800

    started story

--- src/Start.js
+++ src/Start.js
@@ -9,7 +9,9 @@ const Start = () => {
 
   return (
     <div>
-      <h1>Start</h1>
+      <p>Tonight was supposed to be fun--it was Edd's birthday party. It was supposed to be about Edd. <br />BUT Jimmy "Hook" Baggins made it about him.
+      He's missing. I'm basically a detective. My name is Katie "Sherlock" Westbrook. This is my assistant. Yes, he's a cat but he's also a aspiring model. What is your name? </p>
+
     </div>
   );
 }
