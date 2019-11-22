34bbbfaaa5c1c45052a06bbe1676c330ac501ba2
k-westbrook <kcheriewestbrook@gmail.com>
2019-11-22 14:13:14 -0800

    style

--- src/Start.js
+++ src/Start.js
@@ -3,17 +3,60 @@ import logo from './logo.svg';
 import './App.css';
 import Routes from './Routes.js'
 import { BrowserRouter as Router, Link } from 'react-router-dom'
+import { withStyles } from '@material-ui/core/styles';
+import { sizing, border, borderColor } from '@material-ui/system';
+import { Typography, InputLabel, Input } from '@material-ui/core';
 
 
-const Start = () => {
 
+const styles =
+{
+  pageContainer:
+  {
+    display: 'flex',
+    flexDirection: "column"
+  },
+  nameForm:
+  {
+    backgroundColor: '#fafafa',
+    width: '30vw'
+  },
+  formContainer:
+  {
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: "5vh"
+  },
+  input:
+  {
+    width: '100%',
+    fontSize: '100%',
+    padding: '1vh'
+  },
+  inputLabel:
+  {
+    fontSize: '30%'
+  }
+}
+
+const Start = (props) => {
+  const { classes } = props;
   return (
-    <div>
-      <p>Tonight was supposed to be fun--it was Edd's birthday party. It was supposed to be about Edd. <br />BUT Jimmy "Hook" Baggins made it about him.
-      He's missing. I'm basically a detective. My name is Katie "Sherlock" Westbrook. This is my assistant. Yes, he's a cat but he's also a aspiring model. What is your name? </p>
+    <div className={classes.pageContainer}>
+      <div>
+        <Typography variant='p'>Tonight was supposed to be fun--it was Edd's birthday party. It was supposed to be about Edd. <br />BUT Jimmy "Hook" Baggins made it about him.
+      He's missing. I'm basically a detective. My name is Katie "Sherlock" Westbrook. This is my assistant. Yes, he's a cat but he's also a aspiring model. What is your name? </Typography>
+
+      </div>
+      <div className={classes.formContainer}>
+        <form className={classes.nameForm}>
+          <InputLabel htmlFor="userName">Name</InputLabel>
+          <Input className={classes.input} id="userName" name='userName'></Input>
+        </form>
+      </div>
 
     </div>
   );
 }
 
-export default Start;
+export default withStyles(styles)(Start);
