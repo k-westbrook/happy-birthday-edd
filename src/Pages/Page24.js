5983e7c25945216010956782830683b15c46e45c
k-westbrook <kcheriewestbrook@gmail.com>
2019-11-22 19:07:49 -0800

    prison end

---
+++ src/Pages/Page24.js
@@ -0,0 +1,105 @@
+import React from 'react';
+
+import { withStyles } from '@material-ui/core/styles';
+import { Typography, InputLabel, Input, Button } from '@material-ui/core';
+import { endGameBad } from '../Store/Game'
+import { connect } from 'react-redux'
+
+
+
+
+const styles =
+{
+  pageContainer:
+  {
+    display: 'flex',
+    flexDirection: "column",
+    width: '100vw'
+  },
+  nameForm:
+  {
+
+    width: '30vw'
+  },
+  formContainer:
+  {
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: "5vh"
+  },
+  input:
+  {
+    width: '100%',
+    fontSize: '100%',
+    padding: '1vh',
+    backgroundColor: 'white',
+  },
+  inputLabel:
+  {
+    fontSize: '100%',
+    color: 'light-grey',
+    backgroundColor: 'white',
+  },
+  button:
+  {
+    backgroundColor: 'white',
+    width: "50%",
+    marginTop: '2vh'
+  },
+  buttonGroup:
+  {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  }
+}
+
+
+
+const Page8 = (props) => {
+  const { classes } = props;
+  const endGame = () => {
+
+    props.history.push(`/end`)
+  }
+
+
+  return (
+
+    < div className={classes.pageContainer} >
+      {
+        props.gameState.userName ?
+          <div>
+            <Typography variant='h3'>THE END</Typography>
+            <Typography variant='body1'> Well it backfires, I told you as a Lifetime watcher. You are basically living your life like you are on Lifetime and getting framed. And that is what happens. Edd accuses you because you are waving your finger like you own his party. He runs to his room and calls the police. Paula ties your hands behind your back because she's bitter about you rejecting her beer. Police show and and recognize you from the crime scene. Your story doesn't hold up and you get booked. I come visit you in jail and show you pictures of Pablo. I WILL GET THE MURDERER I PROMISE.</Typography>
+            <div className={classes.buttonGroup}>
+              <Button className={classes.button} onClick={endGame}>GO BACK TO START</Button>
+            </div>
+
+
+          </div>
+          :
+          <div>
+            <Typography variant='h3'>Page Loading...</Typography>
+          </div>
+      }
+    </div >
+  );
+}
+
+
+
+const mapDispatch = dispatch => {
+  return {
+    endGame: () => dispatch(endGameBad())
+  }
+}
+
+const mapState = state => {
+  return {
+    gameState: state.game
+
+  }
+}
+
+export default withStyles(styles)(connect(mapState, mapDispatch)(Page8));
--- src/Routes.js
+++ src/Routes.js
@@ -16,6 +16,7 @@ import Page15 from './Pages/Page15'
 import Page2 from './Pages/Page2'
 import Page22 from './Pages/Page22'
 import Page23 from './Pages/Page23'
+import Page24 from './Pages/Page24'
 
 import Page4 from './Pages/Page4'
 import { connect } from 'react-redux'
@@ -65,6 +66,8 @@ class Routes extends Component {
           <Route exact path="/20" component={Page18} />
           <Route exact path="/22" component={Page22} />
           <Route exact path="/23" component={Page23} />
+          <Route exact path="/24" component={Page24} />
+
 
         </Switch>
       </div>
