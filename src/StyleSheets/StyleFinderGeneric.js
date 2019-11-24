9473ffde10e8c7d4bb22dcf461edbbf388a49f43
k-westbrook <kcheriewestbrook@gmail.com>
2019-11-24 11:12:15 -0800

    updated text

--- src/StyleSheets/StyleFinderGeneric.js
+++ src/StyleSheets/StyleFinderGeneric.js
@@ -138,12 +138,33 @@ export const styleFinder = () => {
       {
         display: 'flex',
         flexDirection: "column",
-        padding: '5vw'
+        width: '100vw',
+        minHeight: '100vh'
       },
-      button:
+      contentContainer:
       {
-        backgroundColor: 'white',
-        width: "50%",
+        marginTop: '5vh',
+        padding: '4vh',
+
+      },
+      title:
+      {
+        marginBottom: '3vh',
+        color: 'red'
+      },
+      text:
+      {
+        color: 'white',
+        borderStyle: 'double',
+        padding: '2vh',
+        minHeight: '20vh',
+        fontSize: '3vh',
+        width: '60vw'
+      },
+      submitButton:
+      {
+        width: "60%",
+        height: '7vh',
         marginTop: '2vh'
       },
       buttonGroup:
@@ -151,6 +172,93 @@ export const styleFinder = () => {
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center'
+      },
+      formContainer:
+      {
+        padding: '3vh',
+        minHeight: '10vh'
+      },
+      nameForm:
+      {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+      },
+      input:
+      {
+        width: '80%',
+        marginBottom: '2vh',
+        color: 'black',
+        fontSize: '4vh'
+      },
+      inputLabel:
+      {
+        fontSize: '4vh',
+        marginBottom: '3vh',
+        color: 'black'
+      },
+      storyContent: {
+        color: 'white',
+        fontSize: '2.5vh',
+        marginBottom: '6vh'
+      },
+      pageTitle:
+      {
+        color: 'red',
+        marginBottom: '3vh',
+        fontSize: '10vh'
+      },
+      storyContainer:
+      {
+        marginTop: '5vh',
+        padding: '2vh'
+      },
+      button: {
+        width: '80%',
+        marginBottom: '3vh'
+      },
+      endPageContainer: {
+        background: 'linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 58%, rgba(253,64,39,1) 82%, rgba(252,176,69,1) 100%)',
+        display: 'flex',
+        flexDirection: "column",
+        width: '100vw',
+        minHeight: '100vh'
+
+      },
+      endContent:
+      {
+        marginTop: '5vh',
+        padding: '3vh'
+      },
+      endTitle:
+      {
+        color: 'white',
+        marginBottom: '2vh',
+        borderStyle: 'solid',
+        padding: '4vh',
+        textAlign: 'center'
+      },
+      endText:
+      {
+        fontSize: '3vh',
+        marginBottom: '3vh'
+      },
+      paperEnd:
+      {
+        padding: '2vh'
+      },
+      endButton:
+      {
+        width: '80%',
+        fontSize: '2.5vh',
+        marginBottom: '3vh'
+
+      },
+      paper:
+      {
+        margin: '2vh',
+
       }
     }
   }
